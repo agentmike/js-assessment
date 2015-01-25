c734a7a2f72eee2ccff2b7ab752e4559ec5b54ca
Michael <mike.en@gmail.com>
2015-01-25 18:37:20 +0200

    half-way through array.js

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+        return arr.reduce(function(prev,elem) {
+            return prev+elem;
+        },0);
     },
 
     remove : function(arr, item) {
-
+        arr.forEach(function(elem,i,array) {
+            if (elem === item) { array.splice(i,1); }
+        });
+        return arr;
     },
 
     removeWithoutCopy : function(arr, item) {
@@ -19,34 +24,57 @@ define(function() {
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        Array.prototype.push.apply(arr1,arr2);
+        return arr1;
     },
 
     insert : function(arr, item, index) {
-
+        var tail = arr.splice(index,arr.length,item);
+        this.concat(arr,tail);
+        return arr;
     },
 
     count : function(arr, item) {
-
+        var count=0;
+        arr.forEach(function(elem) {
+            if (elem === item) count++;
+        });
+        return count;
     },
 
     duplicates : function(arr) {
+        var map = {};
+        var self = this;
+        arr.forEach(function(elem,index,array) {
+            if (typeof map[elem] === 'undefined') {
+                map[elem] = 1;
+            } else {
+                map[elem]++;
+            }
+            if (map[elem] > 1) array.splice(index,1);
+        });
+        return arr;
+
 
     },
 
